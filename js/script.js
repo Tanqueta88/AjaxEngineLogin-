a411d2bd42c7b2c0520eba11a38093a062c7da6d
tanqueta88 <oliverprim783@gmail.com>
2023-07-12 23:58:05 -0300

    Function verifyUser is added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let credenciales = {
+let credentials = {
     user: false,
     password: false
 
@@ -7,10 +7,51 @@ let credenciales = {
 
 let http;
 
-let intentos = 3;
+let Attempts = 3;
 
-let regExpMail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-var expresionRegular = /^\d{1,2}$/;
+let regExpemail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+var regularPhrase = /^\d{1,2}$/;
 
-let clave = document.getElementById("password");
-let mail = document.getElementById("mail");
+let password = document.getElementById("password");
+let email = document.getElementById("email");
+
+
+function verifyUser() {
+
+    let wrongEmail = document.getElementById("wrongEmail");
+
+
+    if (regExpemail.test(email.value)) {
+
+        console.log("valid email");
+        credentials.user = true;
+        wrongEmail.innerHTML = "";
+    } else {
+        wrongEmail.innerHTML = "*User must be an email*";
+    }
+
+}
+
+function verifyPassword() {
+
+    let wrongEmail = document.getElementById("wrongEmail");
+
+    if (regularPhrase.test(password.value)) {
+
+        console.log("Invalid password");
+        credentials.password = true;
+        wrongPassword.innerHTML = "";
+    } else {
+        wrongPassword.innerHTML = "*Password must contain one or two digits!*";
+    }
+}
+
+
+
+function validateCredentials() {
+
+    initialize();
+    let id = document.getElementById('password').value;
+    requestSend('https://jsonplaceholder.typicode.com/users/', id, action, 'GET');
+
+}
