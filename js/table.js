de2a3312dfa05772461d18deb213e2a4788b9ad8
tanqueta88 <oliverprim783@gmail.com>
2023-07-21 22:54:40 -0300

    The requestSend function is added

--- js/table.js
+++ js/table.js
@@ -6,3 +6,19 @@ function obtenerData() {
     var id = document.getElementById('ID').value;
     requestSend('https://rickandmortyapi.com/api/character/', id, action, 'GET');
 }
+
+function inicializar() {
+    if (window.XMLHttpRequest) {
+        http = new XMLHttpRequest();
+    } else {
+        http = new ActiveXObject();
+    }
+}
+
+function requestSend(url, id, funcionActuadora, metodo) {
+
+    http.onreadystatechange = funcionActuadora;
+    http.open(metodo, url + id, true);
+    http.send();
+
+}
